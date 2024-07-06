f603ffbc62d19fab305ad9ff0239550100ba83de
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-07-06 09:50:22 -0300

    Ajustando media querie

--- index.php
+++ index.php
@@ -26,50 +26,44 @@ $result_bebibas = $mysqli->query($sql);
 <body>
 
     <div class="home">
-        <a href="./index.php" style="padding: 10px 20px;">
+        <a href="./index.php">
             <img src="./imagens/imagens_pincipal/home_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" height="30px" width="30px" alt="Home">
         </a>
     </div>
-    <div class="shopCart">
-        <a href="./carrinho.php" style="padding: 10px 20px;">
-            <img src="./imagens/imagens_pincipal/shopping_cart_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" height="30px" width="30px" alt="Carrinho">
-        </a>
-    </div>
-    <div style="position: fixed; right:15px; top:20px; z-index: 9999;">
+    <div style="position: fixed; right:15px; top:20px; z-index: 9999;" class="openOrClosedDiv">
         <span class="openOrClosed">fechado</span>
     </div>
     <?php if(!isset($_SESSION['user'])):?>
         <div class="logout" style="display: none;">
-            <a href="./logout_usuario.php" style="color: white; padding: 10px 20px; font-weight: bold; text-decoration: none;">Sair</a>
         </div>
     <?php else: ?>
         <div class="logout" style=" position: fixed; z-index: 9999;">
-            <a href="./logout_usuario.php" style="color: white; padding: 10px 20px; font-weight: bold; text-decoration: none;">Sair</a>
+            <a href="./logout_usuario.php">Sair</a>
         </div>
     <?php endif; ?>
 
     <header>
-        <h1>Quentinhas da Vanessa</h1>
+        <h1 class="nomeLoja">Quentinhas da Vanessa</h1>
     </header>
 
     <section class="agarra">
 
     </section>
     <main>
-        <div style="left: 45%; top: 20%; margin: auto; position: absolute;">
-            <img src="./imagens/imagens_pincipal/comida-criolla-peru-peruvian-food-260nw-2191344515.webp" style="border-radius: 50%; height: 150px; width: 150px; border: 1px solid white; box-shadow: 2px 2px 8px black;" alt="Logo do Restaurante">
-            <p style="transform: translate(-50px, 0);">Restaurante • 20-90 min • <span class="sit"></span></p>
-            <p style="transform: translate(-40px, 25px);">Horário de funcionamento hoje:</p>
-            <p style="transform: translate(20px, 30px);"><strong>11:00 às 15:00</strong></p>
-            <hr style="transform: translate(-80px, 30px);">
+        <div class="infoPrincipal">
+            <img src="./imagens/imagens_pincipal/comida-criolla-peru-peruvian-food-260nw-2191344515.webp" alt="Logo do Restaurante" class="logoRestaurante">
+            <p class="tempoDeEntrega">Restaurante • 20-90 min • <span class="sit"></span></p>
+            <p class="horarioDeFuncionamento">Horário de funcionamento hoje:</p>
+            <p class="hora"><strong>11:00 às 15:00</strong></p>
+            <hr class="hr">
             <?php if(!isset($_SESSION['user'])):?>
                 <a href="./login_usuario.php" class="logBtn">FAÇA LOGIN</a>
             <?php else: ?>
-                <p style="transform: translate(35px, 35px); font-weight: bold;"> Olá, <?php echo $_SESSION['nome']?>!</p>
+                <p class="nomeDoCliente"> Olá, <?php echo $_SESSION['nome']?>!</p>
             <?php endif; ?>
         </div>
-        <section style="padding: 15px;">
-            <h1 style="padding-top: 250px; margin-bottom: 15px; color: darkslategray; margin-left: 15px">ESCOLHA O SEU PRATO</h1>
+        <section style="padding: 15px;" class="secPratos">
+            <h1 class="pratosTitle">ESCOLHA O SEU PRATO</h1>
             <div class="pratosContainer">
                 <?php while($row = $result->fetch_assoc()) { ?>
                     <a href="./complemento_produto.php?comprar=<?php echo $row['id'];?>" class="linkPratos">
@@ -77,12 +71,14 @@ $result_bebibas = $mysqli->query($sql);
                             <div class="imgPrato">
                                 <img height="80px" width="80px" src="./conexao/<?php echo $row['imagem'];?>" alt="">
                             </div>
-                            <div class="info">
-                                <h1><?php echo $row['nome']?></h1>
-                                <p style="color: rgb(119, 119, 119);">(<?php echo $row['descricao']?>)</p>
-                            </div>
-                            <div class="preco" style="display: block; width: 100%; font-weight: bold; color: #27A143; font-size: 1.3em;">
-                                <p style="float: right;"><?php echo "R$" . number_format($row['preco'], 2, "," , ".")?></p>
+                            <div class="infoGeral">
+                                <div class="info">
+                                    <h1 class="nomeDoPrato"><?php echo $row['nome']?></h1>
+                                    <p style="color: rgb(119, 119, 119);" class="descricaoDoPrato">(<?php echo $row['descricao']?>)</p>
+                                </div>
+                                <div class="preco">
+                                    <p><?php echo "R$" . number_format($row['preco'], 2, "," , ".")?></p>
+                                </div>
                             </div>
                         </div>
                     </a>
@@ -90,7 +86,7 @@ $result_bebibas = $mysqli->query($sql);
             </div>
         </section>
         <section style="padding: 15px;">
-            <h1 style="margin-bottom: 15px; color: darkslategray; margin-left: 15px">BEBIDAS</h1>
+            <h1 class="titleBebidas">BEBIDAS</h1>
                 <div class="pratosContainer">
                 <?php while($row = $result_bebibas->fetch_assoc()) { ?>
                     <a href="./complemento_produto.php?comprar=<?php echo $row['id'];?>" class="linkPratos">
@@ -98,12 +94,14 @@ $result_bebibas = $mysqli->query($sql);
                             <div class="imgPrato">
                                 <img height="80px" width="80px" src="./conexao/<?php echo $row['imagem'];?>" alt="">
                             </div>
-                            <div class="info">
-                                <h1><?php echo $row['nome']?></h1>
-                                <p style="color: rgb(119, 119, 119);">(<?php echo $row['descricao']?>)</p>
-                            </div>
-                            <div class="preco" style="display: block; width: 100%; font-weight: bold; color: #27A143; font-size: 1.3em;">
-                                <p style="float: right;"><?php echo "R$" . number_format($row['preco'], 2, "," , ".")?></p>
+                            <div class="infoGeral">
+                                <div class="info">
+                                    <h1 class="nomeDoPrato"><?php echo $row['nome']?></h1>
+                                    <p style="color: rgb(119, 119, 119);" class="descricaoDoPrato">(<?php echo $row['descricao']?>)</p>
+                                </div>
+                                <div class="preco">
+                                    <p><?php echo "R$" . number_format($row['preco'], 2, "," , ".")?></p>
+                                </div>
                             </div>
                         </div>
                     </a>
@@ -111,6 +109,11 @@ $result_bebibas = $mysqli->query($sql);
                 </div>
         </section>
     </main>
+    <div class="shopCart">
+        <a href="./carrinho.php">
+            <img src="./imagens/imagens_pincipal/shopping_cart_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" height="30px" width="30px" alt="Carrinho">
+        </a>
+    </div>
     <script src="./js_pincipal/funcoes.js"></script>
 </body>
 </html>
