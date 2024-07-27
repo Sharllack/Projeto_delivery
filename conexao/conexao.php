c20587ed8ecf89aeb2332d1c44ad225bba75b1f0
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-07-26 23:47:13 -0300

    novo repositório

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $usuario = 'root';
 $senha = 'LoideMartha12*';
-$database = 'venda_produtos';
+$database = 'delivery';
 $host = 'localhost';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
