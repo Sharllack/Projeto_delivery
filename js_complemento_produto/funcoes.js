4268092f62ceab48de4586eacdae4012456c857c
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-07-29 15:22:26 -0300

    Update funcoes.js

--- js_complemento_produto/funcoes.js
+++ js_complemento_produto/funcoes.js
@@ -13,3 +13,24 @@ function atualizarSituacao() {
 
 atualizarSituacao(); // Chama a função imediatamente ao carregar a página
 setInterval(atualizarSituacao, 60000); // Chama a função a cada minuto
+
+
+//------------ DARK MODE -------------
+const slidePref = localStorage.getItem('slide');
+const main = document.querySelector('main');
+
+const darkModePreference = localStorage.getItem('darkMode');
+if (darkModePreference === 'on') {
+      main.style.backgroundColor = "#000000e8";
+      main.style.transition = '.5s';
+      main.style.color = "white";
+
+      localStorage.setItem('darkMode', 'on');
+
+    toggle.checked = true;
+    
+} else {
+      main.style.backgroundColor = "";
+      main.style.transition = '.5s';
+      main.style.color = "black";
+}
