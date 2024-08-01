a6de54c08c891f92c27d05f0cb28608c4c41ee17
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-08-01 08:10:09 -0300

    Update funcoes.js

--- js_pincipal/funcoes.js
+++ js_pincipal/funcoes.js
@@ -1,29 +1,3 @@
-function atualizarSituacao() {
-    const sit = document.querySelector('.sit');
-    const carrinho = document.querySelector('.carrinho');
-    const link = document.querySelectorAll('.linkPratos');
-    const log = document.querySelector('.logBtn');
-    const situ = document.querySelector('.situ').innerHTML;
-    const hora = new Date().getHours();
-
-    if (hora >= 11 && hora < 15 || situ === "Aberto") {
-        sit.textContent = 'Aberto';
-        sit.className = 'open';
-    } else {
-        sit.textContent = 'Fechado';
-        sit.className = 'closed';
-
-        link.forEach(function(link){
-            link.href = "#";
-        })
-        carrinho.href = '#';
-        log.href = '#';
-    }
-}
-
-atualizarSituacao(); // Chama a função imediatamente ao carregar a página
-setInterval(atualizarSituacao, 60000); // Chama a função a cada minuto
-
 setInterval(function() {
     location.reload();
 }, 10000);
@@ -108,6 +82,32 @@ if (darkModePreference === 'on') {
       secBebidas.style.color = "darkslategray";
       secPratos.style.color = "darkslategray";
 }
+
+function atualizarSituacao() {
+    const sit = document.querySelector('.sit');
+    const carrinho = document.querySelector('.carrinho');
+    const link = document.querySelectorAll('.linkPratos');
+    const log = document.querySelector('.logBtn');
+    const situ = document.querySelector('.situ').innerHTML;
+    const hora = new Date().getHours();
+
+    if (hora >= 11 && hora < 15 || situ === "Aberto") {
+        sit.textContent = 'Aberto';
+        sit.className = 'open';
+    } else {
+        sit.textContent = 'Fechado';
+        sit.className = 'closed';
+
+        link.forEach(function(link){
+            link.href = "#";
+        })
+        carrinho.href = '#';
+        log.href = '#';
+    }
+}
+
+atualizarSituacao(); // Chama a função imediatamente ao carregar a página
+setInterval(atualizarSituacao, 60000); // Chama a função a cada minuto
 
 function atualizarTaxa() {
     const taxa = document.querySelector('#taxa').innerHTML;
